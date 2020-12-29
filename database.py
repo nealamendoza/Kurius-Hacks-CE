490cc19f60545e2b1232feb4d296291d15acabe2
kylejava <kyleedwardgjava@gmail.com>
2020-12-28 19:24:36 -0800

    Adding functions to database.py, created cluster in MongoDB

---
+++ database.py
@@ -0,0 +1,37 @@
+import pymongo
+from keys import *
+from pymongo import MongoClient
+
+db = cluster["KuriusHacks"]
+collection = db["Events"]
+
+"""
+What it does:
+    - This function adds a new event to the database
+      It will add the name of the event, who, when, etc.
+
+Parameters:
+    - Name of event (ex: "CSUSM Food Drive")
+    - Where is the event (ex: "123 Cougar Lane")
+    - When is the event
+
+Return:
+    - Does not return anything
+"""
+
+
+def add_Event_To_Database(nameOfEvent, addressOfEvent, dateOfEvent, descOfEvent, contactInfo):
+    FoodDriveEvent = {
+        'nameOfEvent': nameOfEvent,
+        'addressOfEvent': addressOfEvent,
+        'dateOfEvent': dateOfEvent,
+        'descOfEvent': descOfEvent,
+        'contactInfo': contactInfo
+    }
+    collection.insert_one(FoodDriveEvent)
+    print("Added event to database")
+
+
+
+def preview_database():
+    pass
