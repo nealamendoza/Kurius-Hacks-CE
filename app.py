8e40af516382ba53d4642300405a4ba1036eb64a
kylejava <kyleedwardgjava@gmail.com>
2020-12-30 01:56:13 -0800

    Added Photos

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 
 import requests
-from flask import Flask
-
+from flask import Flask, render_template, flash, request
 app = Flask(__name__)
-
-@app.route('/api' , methods = ['GET' , 'POST'])
+from database import *
+@app.route('/' , methods = ['GET' , 'POST'])
 def home():
-    return{
-    'test': "yuh this is a test"
-    }
+    name = ""
+    address = ""
+    date = ""
+    desc = ""
+    contact = ""
+    if (request.method == 'POST'):
+        name = request.form['name']
+        address = request.form['address']
+        date = request.form['date']
+        desc = request.form['desc']
+        contact = request.form['contact']
+        add_Event_To_Database(name , address , date , desc , contact)
+    events = preview_database()
+    return render_template('index.html',events = events)
 
 if __name__ == "__main__":
     app.run(debug=True)
